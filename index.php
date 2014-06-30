7b0e98b7f4a717f1bd08c39cf86f1ef9fc6f1a1a
Marta <marta.preuss@gmail.com>
2014-06-29 21:21:16 -0300

    Troca de fonte,  término de "serviços" e galeria de jobs.

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 	<meta charset="UTF-8">
 	<title>Marta Preuss</title>
 	<link rel="stylesheet" href="css/style.css">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700' rel='stylesheet' type='text/css'>
+	<meta content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" name="viewport">
 </head>
 <body>
 	<div class="topbar"></div>
@@ -13,8 +14,8 @@
 		<h1 class="fl"><a href="#">Marta Preuss</a></h1>
 		<nav class="fr">
 			<ul>
-				<li><a href="#">Twitter</a></li>
-				<li><a href="#">Facebook</a></li>
+				<li><a class="twitter" href="#">Twitter</a></li>
+				<li><a class="fb" href="#">Facebook</a></li>
 				<li><a class="openMenu" href="#menu">Menu</a></li>
 			</ul>
 		</nav>
@@ -43,36 +44,36 @@
 	<section id="services">
 		<div class="col clearfix">
 		<h2>Serviços</h2>
-		<ul>
-			<li>
-				<img src="" alt="">
+		<ul class="servicos">
+			<li class="design">
+				<div class="ico"></div>
 				<h3>Web design</h3>
-				<p></p>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even </p>
 			</li>
-			<li>
-				<img src="" alt="">
+			<li class="webdev">
+				<div class="ico"></div>
 				<h3>Web development</h3>
-				<p></p>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even </p>
 			</li>
-			<li>
-				<img src="" alt="">
+			<li class="responsivo">
+				<div class="ico"></div>
 				<h3>Responsivo</h3>
-				<p></p>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even </p>
 			</li>
-			<li>
-				<img src="" alt="">
+			<li class="crossbrowser">
+				<div class="ico"></div>
 				<h3>Cross-browser</h3>
-				<p></p>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even </p>
 			</li>
-			<li>
-				<img src="" alt="">
+			<li class="wp">
+				<div class="ico"></div>
 				<h3>Wordpress</h3>
-				<p></p>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even </p>
 			</li>
-			<li>
-				<img src="" alt="">
+			<li class="emailmkt">
+				<div class="ico"></div>
 				<h3>E-mail marketing</h3>
-				<p></p>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even </p>
 			</li>			
 		</ul>
 		</div>
@@ -80,7 +81,7 @@
 	<section id="projects">
 		<div class="col clearfix">
 		<h2>Projetos</h2>
-		<ul>
+		<ul id="projetos">
 			<li><a target="_blank" href="http://estadao.com.br" class="projeto estado"><span>O front-end do novo portal do Estadão teve a minha participação, em uma equipe de excelentes profissionais. Defini os helpers, estilos principais, grids. HTML, CSS e JS.</span></a></li>
 			<li><a target="_blank" href="http://clickon.com.br" class="projeto clickon"><span>Fui responsável pela manutenção do front-end do ClickOn, além de implementar novos layouts e funcionalidades. Fiz o layout para web e ajustei em dispositivos móveis.</span></a></li>
 			<li><a target="_blank" href="http://dudi.com.br" class="projeto dudi"><span>Fui responsável pela manutenção do front-end do dudi, além de implementar novos layouts e funcionalidades. Fiz o layout para web e ajustei em dispositivos móveis.</span></a></li>
@@ -95,15 +96,76 @@
 			<li><a target="_blank" href="http://www.cegoemtiroteio.com/" class="projeto cet"><span>O Cego em Tiroteio precisava de um layout que fosse útil para cegos e bonito para quem vê. Fiz o layout e o tema para WordPress.</span></a></li>
 		</ul>
 		</div>
+		<article class="box" id="hml">
+			<a href="#" title="Fechar" class="close">X</a>
+			<h3>Sites em homologação</h3>
+			<p>Esses sites não foram para o ar, mas eu fiz e tenho orgulho de mostrar:</p>
+			<ol>
+				<li><a href="http://marta.preuss.nom.br/tudo/becart" target="_blank">becart</a></li>
+				<li><a href="http://marta.preuss.nom.br/tudo/cmis" target="_blank">cmis</a></li>
+				<li><a href="http://marta.preuss.nom.br/tudo/energia" target="_blank">rede energia</a></li>
+				<li><a href="http://marta.preuss.nom.br/tudo/lambda" target="_blank">lambda</a></li>
+				<li><a href="http://marta.preuss.nom.br/tudo/modalize" target="_blank">modalize</a></li>
+				<li><a href="http://marta.preuss.nom.br/tudo/tstClick" target="_blank">clickon viagens</a></li>
+			</ol>
+		</article>
+		<article class="box" id="resenhas">
+			<a href="#" title="Fechar" class="close">X</a>
+			<h3>Textos diversos</h3>
+			<p>Além dos contos, tenho também alguns textos institucionais, geralmente reviews. Estão em PDF.</p>
+			<ol>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/50%20anos%20de%20rock.pdf"> 50 anos de rock</a></li>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/A%20arte%20do%20insulto.pdf"> A arte do insulto</a></li>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/Apenas%20o%20fim.pdf"> Apenas o fim</a></li>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/Era%20do%20gelo%203.pdf"> Era do gelo 3</a></li>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/No%3f%20No%3f.pdf">Aventura nos sete mares</a></li>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/Risoto%20com%20Rodo.pdf"> Risoto com Rodo</a></li>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/Safari%204.0.pdf"> Safari 4.0</a></li>
+				<li><a target="_blank" href="http://tudo.marta.preuss.nom.br/textos/The%20sims%203.pdf"> The sims 3</a></li>
+			</ol>
+		</article>
 	</section>
 	<section id="education">
 		<div class="col">
 		<h2>Formação</h2>
+			<img src="http://marta.preuss.nom.br/img/perfil.png" alt="Marta Preuss">
+			<p>Tenho descoberto e construido quem eu sou me baseando em dogmas que fazem sentido para mim. Trate os outros como a si mesmo;
+			seja gentil; viva uma vida simples; nem todos têm as mesmas oportunidades e benefícios que eu; preconceitos devem ser destruídos
+			em nome da felicidade geral da humanidade; julgar as pessoas não acrescenta em nada; o tempo é curto para perder com bobagem.</p>
+			<p>(cada um escolhe o que é "bobagem".)</p>
+			<p>Gosto muito de ler. Meu cérebro consegue filtrar como eu imagino as coisas quando leio. Não sou forçada, como na mídia visual, a ver
+			coisas que não quero (o que não significa que não existam livros que - nossa - são super fortes, mas você entendeu). Gosto de parques,
+			pra sair, andar, tirar fotos, descansar. Gosto de cachorros e gatos.</p>
+			<p>Já não sei mais se sou chata ou não, mas tenho me prendido às minhas convicções cada vez mais. Acredito em quem eu sou e no 
+			que eu faço, e tudo que faço é com amor. Não tem como dar errado ;)</p>
+
+			<h3>Formação</h3>
+			<p>Em resumo: Bacharel em Comunicação Social com Habilitação em Mídias Digitais pela Metodista (2008) e inglês pela WiseUp (2010), além de um monte de palestras e workshops.</p>
+			<p>Se você quiser, pode ver <a href="http://meucv.co/mapreuss">meu currículo</a> ou acompanhar minha trajetória pela linha do tempo:</p>
+
+			<iframe src="http://embed.verite.co/timeline/?source=0AmQtQE217BaqdFcwa0RNT1dVLWlhcnJGaUdvcF9vakE&font=Bevan-PotanoSans&maptype=toner&lang=pt-br&start_at_end=true&height=650" frameborder="0" height="650px" width="100%"></iframe>
 		</div>
 	</section>
 	<section id="contact">
 		<div class="col">
 		<h2>Contato</h2>
+			<p>Se você quiser bater papo, marcar um café ou falar sobre negócios, pode me mandar uma mensagem no <a href="mailto:marta.preuss@gmail.com">marta.preuss@gmail.com</a> ou me achar nas redes sociais abaixo:</p>
+			<ul class="social">
+				<li><a title="Twitter" href="http://twitter.com/suco_de_uva" target="_blank">Twitter</a></li>
+				<li><a title="Facebook" href="http://facebook.com/mapreuss" target="_blank">Facebook</a></li>
+				<li><a title="Flickr" href="http://flickr.com/mapreuss" target="_blank">Flickr</a></li>
+				<li><a href="http://instagram.com/mapreuss" target="_blank">Instagram</a></li>
+				<li><a href="http://www.lastfm.com.br/user/mapreuss" target="_blank">Last.fm</a></li>
+				<li><a title="Codepen" href="http://codepen.io/mapreuss" target="_blank">Codepen</a> </li>
+				<li><a title="Steam" href="http://steamcommunity.com/id/suco_de_uva/" target="_blank">Steam</a></li>
+				<li><a title="Steam" href="http://www.skoob.com.br/estante/livros/todos/277235" target="_blank">Skoob</a></li>
+			</ul>
+			<hr>
+			<p>Você ainda pode acompanhar meu trabalho nos sites que eu cuido ou participo:</p>
+			<ul>
+				<li><a target="_blank" href="http://marcandolivro.tumblr.com">Marcando Livro</a></li>
+				<li><a target="_blank" href="http://blog.marta.preuss.nom.br">Blog pessoal</a></li>
+			</ul>
 		</div>
 	</section>
 	<footer>
--- js/script.js
+++ js/script.js
@@ -2,7 +2,80 @@ $(document).ready(function(){
 	$(".openMenu").click(function(){
 		var menu = $(this).attr('href');
 		$(menu).toggleClass("open");
+		$(this).toggleClass("ativo");
 		return false;
 	});
+	ScrollJobs('projetos');
 });
+
+function ScrollJobs(id){
+	var id = "#"+id;
+	var larguraItem = $(id + " li").outerWidth();
+	var qdadeItem = $(id + " li").length;
+	var thumbsVisiveis = Math.round($(id).parent().outerWidth() / larguraItem);
+	
+	$(id).css({
+		"width":qdadeItem*larguraItem,
+		"left":0});
+
+	$(id).wrap("<div class='mask'></div>");
+	$(id).parent(".mask").css({
+		"overflow":"hidden",
+		"margin":"auto",
+		"height": $(id + " li").height(),
+		"width":thumbsVisiveis*larguraItem
+	});
+	$(id).parent(".mask").wrap("<div class='carrossel'></div>")
+
+	$(id).parent().parent().prepend("<div class='before'>Anterior</div>");
+	$(id).parent().parent().append("<div class='after'>Próximo</div>");
+
+	var btnVoltar = $(id).parent().parent().find(".before");
+	var btnAvancar = $(id).parent().parent().find(".after");
+
+	$(btnVoltar).css({
+		"cursor":"auto",
+		"opacity":0.5
+	})
+
+	$(btnVoltar).click(function(){		
+		if (!$(id).is(':animated')) {
+			var pos = parseInt($(id).css("left"));
+			if(pos < 0){
+				$(id).animate({"left":"+="+thumbsVisiveis*larguraItem},500);
+				var pos = parseInt($(id).css("left"));
+				$(btnAvancar).css({
+						"cursor":"pointer",
+						"opacity":1
+					})
+			} else {
+				$(this).css({
+					"cursor":"auto",
+					"opacity":0.5
+				});
+				return false;
+			}
+		}
+	});
+	$(btnAvancar).click(function(){	
+		if (!$(id).is(':animated')) {
+			var pos = parseInt($(id).css("left"));
+			if(pos > (larguraItem*qdadeItem)*-1+(thumbsVisiveis*larguraItem)){
+				$(id).animate({"left":"-="+thumbsVisiveis*larguraItem},500);
+				$(btnVoltar).css({
+						"cursor":"pointer",
+						"opacity":1
+					})
+			} else {				
+				$(this).css({
+					"cursor":"auto",
+					"opacity":0.5
+				});
+				return false;
+			}
+		}
+	});
+
+	
+}
 
